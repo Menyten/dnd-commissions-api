508783fa95ecf05656dddd37945f33f98751e268
joel.pedersen97@gmail.com <joel.pedersen97@gmail.com>
2020-06-01 21:06:05 +0200

    error handling for get token function

--- helpers/getToken.js
+++ helpers/getToken.js
@@ -1,9 +1,8 @@
 export default cookies => {
-  if (!cookies) {
-    return;
-  }
-  return cookies
+  if (!cookies) return;
+  const cookie = cookies
     .split(' ')
-    .filter(cookie => cookie.includes('dnd_commissions'))[0]
-    .split('=')[1];
+    .filter(cookie => cookie.includes('dnd_commissions'))[0];
+  if (!cookie) return;
+  return cookie.split('=')[1];
 };
